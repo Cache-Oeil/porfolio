914052c4e6f31ae0c0736e37659c3c4dcaa90035
CHAU Khac Quoc Bao <bao.chau@netdevices.fr>
2018-05-09 17:27:45 +0200

    update config files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -32,7 +32,13 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [ 'sass-loader' ]
+        use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
       }
     ]
   }
