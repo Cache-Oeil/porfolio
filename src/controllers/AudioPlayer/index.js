f06bfeb41c5219a9913cb511c61fe98c8ea80522
Bao CHAU <baochow95@gmail.com>
2018-05-25 01:57:39 +0200

    add Progress controller

---
+++ src/controllers/AudioPlayer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import AudioPlayer from '../../components/AudioPlayer';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AudioPlayer);
