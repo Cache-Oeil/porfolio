ea6d8c6c2ea739063f07f14da583de81a892515e
Bao Chau <bao.chau@netdevices.fr>
2018-06-16 14:21:41 +0200

    replace all button control

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -1,18 +1,53 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import Progress from './Progress';
+import IconButton from 'material-ui/IconButton';
+import Repeat from 'material-ui/svg-icons/av/repeat';
+import Loop from 'material-ui/svg-icons/av/loop';
+import SkipPrevious from 'material-ui/svg-icons/av/skip-previous';
+import SkipNext from 'material-ui/svg-icons/av/skip-next';
+import FastForward from 'material-ui/svg-icons/av/fast-forward';
+import FastRewind from 'material-ui/svg-icons/av/fast-rewind';
+import Replay10 from 'material-ui/svg-icons/av/replay-10';
+import Forward10 from 'material-ui/svg-icons/av/forward-10';
 import './style.scss';
 
-const pulseEffect = (event) => {
-  const elementChild = event.currentTarget.getElementsByTagName('div')[0]
-  elementChild.style.animation = "pulse2 0.3s linear"
-  setTimeout(() => elementChild.style.animation = "none", 300)
-}
+const styles = {
+  smallIcon: {
+    width: 36,
+    height: 36,
+  },
+  mediumIcon: {
+    width: 48,
+    height: 48,
+  },
+  largeIcon: {
+    width: 60,
+    height: 60,
+  },
+  small: {
+    width: 72,
+    height: 72,
+    padding: 16,
+  },
+  medium: {
+    width: 96,
+    height: 96,
+    padding: 24,
+  },
+  large: {
+    width: 120,
+    height: 120,
+    padding: 30,
+  },
+};
+
 class AudioPlayer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loop: true,
+      loop: false,
+      repeat: true,
       play: true,
       selectedTrack: this.props.tracks[0],
       currentTime: 0
@@ -21,7 +56,6 @@ class AudioPlayer extends Component {
 
   componentDidMount() {
     this.audio && this.audio.play();
-    this.audio.muted = false
   }
 
   updateTime = () => {
@@ -34,6 +68,38 @@ class AudioPlayer extends Component {
   handlePlay = () => {
     this.audio.play();
     this.updateTime()
+  }
+
+  togglePlay = (e) => {
+    if (!this.state.play) {
+      this.handlePlay()
+    } else {
+      this.handlePause()
+    }
+    this.setState({ play: !this.state.play })
+  }
+
+  toggleReplay = () => {
+    const { loop, repeat } = this.state
+
+    if (!loop) {
+      this.audio.loop = !loop
+      this.setState({ loop: this.audio.loop, repeat: false })
+      return
+    }
+    this.audio.loop = false
+    this.setState({ loop: this.audio.loop })
+  }
+
+  toggleRepeat = () => {
+    const { loop, repeat } = this.state
+
+    if (!repeat) {
+      this.audio.loop = false
+      this.setState({ loop: false, repeat: true })
+      return
+    }
+    this.setState({ repeat: false })
   }
 
   nextTrack = () => {
@@ -90,15 +156,6 @@ class AudioPlayer extends Component {
 
   handlePause = () => this.audio.pause() 
 
-  togglePlay = (e) => {
-    if (!this.state.play) {
-      this.handlePlay()
-    } else {
-      this.handlePause()
-    }
-    this.setState({ play: !this.state.play })
-  }
-
   handleSelect = (track) => {
     if (this.state.selectedTrack.id !== track.id) {
       this.setState({ 
@@ -111,6 +168,24 @@ class AudioPlayer extends Component {
     }
   }
 
+  handleEnd = () => {
+    const { tracks } = this.props
+    const { loop, repeat, selectedTrack } = this.state
+
+    if (!repeat && !loop) {
+      return
+    }
+    
+    if (repeat) {
+      if (selectedTrack.id === tracks[tracks.length-1].id) {
+        this.handleSelect(tracks[0])
+        return
+      }
+      this.nextTrack()
+      return
+    }
+  }
+
   componentDidUpdate(prevProps, prevState) {
     if (prevState.selectedTrack.id !== this.state.selectedTrack.id) {
       this.audio.load()
@@ -120,7 +195,7 @@ class AudioPlayer extends Component {
   
   render() {
     const { tracks } = this.props;
-    const { selectedTrack, currentTime } = this.state;
+    const { selectedTrack, currentTime, loop, repeat } = this.state;
     return (
       <div className="audio-player">
         <input type="checkbox" id="play" checked={this.state.play} />
@@ -130,10 +205,32 @@ class AudioPlayer extends Component {
         <div className="throbber"></div>                            
         <label className="play" onClick={this.togglePlay}></label>
         <div className="controls">
-          <div className="control prev" onClick={(e) => { this.prevTrack(); pulseEffect(e) }}><div></div></div>
+          <div className="replay-repeat">
+            <IconButton style={{marginRight: 5}} onClick={(e) => this.toggleReplay()}>
+              <Loop color={loop ? 'white' : '#BDBDBD'} />
+            </IconButton>
+            <IconButton style={{marginLeft: 5}} onClick={(e) => this.toggleRepeat()}>
+              <Repeat color={repeat ? 'white' : '#BDBDBD'} />
+            </IconButton>
+          </div>
+          <div className="control">
+            <IconButton style={styles.small} iconStyle={styles.smallIcon} onClick={(e) => this.prevTrack()}>
+              <SkipPrevious color={'#BDBDBD'} />
+            </IconButton>
+            <IconButton style={styles.small} iconStyle={styles.smallIcon} onClick={(e) => this.backward()}>
+              <Replay10 color={'#BDBDBD'} />
+            </IconButton>
+            <IconButton style={styles.small} iconStyle={styles.smallIcon} onClick={(e) => this.forward()}>
+              <Forward10 color={'#BDBDBD'} />
+            </IconButton>
+            <IconButton style={styles.small} iconStyle={styles.smallIcon} onClick={(e) => this.nextTrack()}>
+              <SkipNext color={'#BDBDBD'} />
+            </IconButton>
+          </div>
+          {/* <div className="control prev" onClick={(e) => { this.prevTrack(); pulseEffect(e) }}><div></div></div>
           <div className="control rw" onClick={(e) => { this.backward(); pulseEffect(e) }}><div></div></div>
           <div className="control fw" onClick={(e) => { this.forward(); pulseEffect(e) }}><div></div></div>
-          <div className="control next" onClick={(e) => { this.nextTrack(); pulseEffect(e) }}><div></div></div>
+          <div className="control next" onClick={(e) => { this.nextTrack(); pulseEffect(e) }}><div></div></div> */}
         </div>
         <ul className="songlist">
           <li className="song"> 
@@ -150,7 +247,7 @@ class AudioPlayer extends Component {
           </li>
           ))}
         </ul>
-        <audio ref={el => this.audio = el} id="audio" style={{display: 'none'}}>
+        <audio ref={el => this.audio = el} id="audio" style={{display: 'none'}} onEnded={this.handleEnd}>
           <source src={selectedTrack.source} />
         </audio>
       </div>
